ed7151a402b3b40372705c36608ce7fbc33a7838
Nicolas Guilloux <novares.x@gmail.com>
2018-07-30 23:53:16 +0200

    Adding user to input group

--- Solus/install_solus.sh
+++ Solus/install_solus.sh
@@ -4,7 +4,7 @@
 # Website: https://nicolasguilloux.eu/
 # Email:   novares.x@gmail.com
 
-# Shadow dependencies: gconf libva-utils libbsd qt5-svg json-c
+# Shadow dependencies: gconf libva-utils libbsd qt5-svg json-cDo
 
 # ----------------------- Dependancies ----------------------- #
 echo "Installing dependencies..."
@@ -29,6 +29,8 @@ echo "Remove temporary files"
 cd ~/
 rm -R /var/tmp/shadow_install
 
+echo "Add the user to the input group"
+sudo usermod -a -G input $USER
 
 ldd -v /opt/Shadow\ Beta/resources/app.asar.unpacked/native/linux/ClientSDL | grep "not found"
 echo "Installation finished. Please reboot your computer."
