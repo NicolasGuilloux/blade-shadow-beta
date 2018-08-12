119c68b4f26b4d3b716eac47bc51fe283527d150
Nicolas Guilloux <novares.x@gmail.com>
2018-08-13 00:42:59 +0200

    Add Linux Mint, edit ReadMe, add Wrapper

--- README.md
+++ README.md
@@ -5,12 +5,18 @@ This repository will gather a bunch of script to install and update Shadow Beta 
 Currently supported:
 - Arch Linux
 - Debian
+- Linux Mint
 
 These scripts are designed by Shadow Discord community and have no link with Blade company.
 Moreover, there is no guarantee that the scripts will work for you.
 
 As you will install programs, you will need the root rights with the "sudo" command. Please install it before running these scripts.
 Moreover, don't start the scripts as root. Use you current user account.
+
+
+# AppImage
+
+Alex is currently building an AppImage that will work on (almost) every distribution. The wrapper will be integrated to detect errors and propose fixes.
 
 
 # Arch Linux
@@ -33,6 +39,11 @@ First, make the script executable: `chmod +x Debian/*`
 To install or update Shadow bêta, simply launch the script and follow it: `Debian/install_debian.sh`.
 
 
+# Linux Mint
+
+Linux Mint needs the extra package `gstreamer1.0-vaapi`. Execute the command: `sudo apt install gstreamer1.0-vaapi`
+
+
 # Solus
 
 **The Solus installation isn't working right now. We're still working on it.**
@@ -42,6 +53,18 @@ A Gist has been open by kabouik: https://gist.github.com/Kabouik/f738c03f5dbb8a3
 To install Shadow beta, go in the Solus folder and make the script executable: `cd Solus && chmod +x *`
 
 Then, launch the script: `./install_solus.sh`
+
+
+# VA Drivers
+
+Keep in mind that you will probably need drivers for VAAPI to make your graphics card well detected. Several options are available based on your GPU.
+
+Please, use the documentation given by your distribution:
+
+- Debian based: https://doc.ubuntu-fr.org/vaapi
+- Arch Linux: https://wiki.archlinux.org/index.php/Hardware_video_acceleration#Installing_VA-API
+
+If a i965 driver is needed on Debian based, install the driver by executing the following command: `sudo apt install i965-va-driver`
 
 
 # Thanks
---
+++ Wrapper/wrapper.pl
@@ -0,0 +1,73 @@
+#!/usr/bin/perl
+
+# Autheur: Nicolas Guilloux
+# Website: https://nicolasguilloux.eu/
+# Email:   novares.x@gmail.com
+
+use strict;
+use warnings;
+
+# alert
+#
+# @param String Title
+# @param String Content
+# @param Bool   (Optional) Question notification
+sub alert {
+    my $command = "notify-send '$_[0]' '$_[1]' -u critical -i shadow-beta";
+    exec($command);
+}
+
+my $errors = '',
+my $errorsCount = 0;
+
+# -------- Vainfo -------- #
+my $vainfo = `vainfo`;
+
+if( $vainfo eq '' ) {
+    $errors .= "Your GPU is not recognized. Please check the `vainfo` command.\n\n";
+    $errorsCount++;
+
+} else {
+
+    # The GPU doesn't support H264.
+    if( index($vainfo, 'H264') == -1 ) {
+        $errors .= "Your GPU does not support any encoding technology used by Shadow. You have to change you GPU to use this application.\n\n";
+        $errorsCount++;
+    }
+
+    if( index($vainfo, 'H265') == -1 and index($vainfo, 'HEVC') == 1 ) {
+        print "Your GPU supports only H264. Do not use H265.\n\n";
+    }
+}
+
+
+# -------- Input --------- #
+my $groups = `groups \$USER`;
+
+if( index($groups, 'input') == -1 ) {
+
+    $errorsCount++;
+
+    print 'Adding the user to the input group.';
+    my $in = `pkexec gpasswd -a \$USER input`;
+
+    $errors .= "The program tried to add the user to the \"input\" group. If you entered the right administrator password, you should reboot to apply the changes. Otherwise, ask the administrator to enter it for you.\n\n";
+}
+
+
+# -------- Xorg check -------- #
+my $env = `echo \$XDG_SESSION_TYPE`;
+chomp $env;
+
+if( $env ne 'x11' ) {
+    $errors .= "Your environnement is not Xorg but is identified as $env. Please switch to Xorg or you will not be able to start this application.";
+    $errorsCount++;
+}
+
+# -------- Start Shadow -------- #
+if( $errorsCount > 0 ) {
+    alert("There is $errorsCount errors", $errors);
+
+} else {
+    system('shadow-beta');
+}
