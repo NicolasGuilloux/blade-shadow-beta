708a90ae3965b8c99307b1fd5e3ab3ab92cdf280
NicolasGuilloux <novares.x@gmail.com>
2018-10-23 09:54:19 +0200

    Update Arch install scripts

--- Arch/install_arch.sh
+++ Arch/install_arch.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-# Install Shadow Beta for Arch Linux
+# Install Shadow Beta for Arch Linux using makepkg
 
 # Author:   Nicolas Guilloux
 # Website:  https://nicolasguilloux.eu/
@@ -10,22 +10,11 @@ set -e
 
 # Contributors: PofMagicfingers
 
-# Dependencies
-#    gconf libnotify libappindicator libxtst nss libcurl-compat libva libdrm freetype2 libbsd json-c opus sdl gcc7-libs ttf-dejavu
+# Move to the AUR folder
+cd AUR
 
-# ----------------------- Dependancies ----------------------- #
+# Build the package
+makepkg
 
-echo "Installing dependencies..."
-sudo pacman --needed -S gconf libnotify libappindicator libxtst nss libcurl-compat libva libdrm freetype2 libbsd json-c opus sdl gcc7-libs ttf-dejavu
-
-echo "Correct Libjson-c"
-sudo ln -f /usr/lib/libjson-c.so /usr/lib/libjson-c.so.3
-
-echo "Correct Libubsan"
-sudo cp /usr/lib/gcc/x86_64-pc-linux-gnu/7.3.1/libubsan.so.0 /usr/lib/
-
-echo "Fix the font issue"
-sudo ln -sf /usr/share/fonts/TTF /usr/share/fonts/truetype
-
-echo "Install the new version of Shadow"
-exec ./update_arch.sh
+# Install the package
+sudo pacman -U ./shadow-beta-*-x86_64.pkg.tar.xz
